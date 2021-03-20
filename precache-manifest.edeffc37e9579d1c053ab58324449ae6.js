78518ea874f83cdcd66be6fb7aa41b0df133f62a
FranHainesZone <52914701+FranHainesZone@users.noreply.github.com>
2021-03-20 16:54:57 +0000

    Updates

---
+++ precache-manifest.edeffc37e9579d1c053ab58324449ae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.53ebc025.js"
+  },
+  {
+    "revision": "4729442ed181fd38a5cb",
+    "url": "/portfolio/static/js/main.4729442e.chunk.js"
+  },
+  {
+    "revision": "4dbfbdec9ea871f99630",
+    "url": "/portfolio/static/js/2.4dbfbdec.chunk.js"
+  },
+  {
+    "revision": "4729442ed181fd38a5cb",
+    "url": "/portfolio/static/css/main.8b1d7a64.chunk.css"
+  },
+  {
+    "revision": "4dbfbdec9ea871f99630",
+    "url": "/portfolio/static/css/2.d64aa802.chunk.css"
+  },
+  {
+    "revision": "34cab85c2d658d5e681d91327f7848a0",
+    "url": "/portfolio/index.html"
+  }
+];
