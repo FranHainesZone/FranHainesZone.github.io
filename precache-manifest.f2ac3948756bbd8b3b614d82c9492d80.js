71caa02866f81b293460d23e7aa7b4cd8e3a5a03
FranHainesZone <52914701+FranHainesZone@users.noreply.github.com>
2021-04-13 08:12:42 +0100

    Updates

--- precache-manifest.f2ac3948756bbd8b3b614d82c9492d80.js
+++ precache-manifest.f2ac3948756bbd8b3b614d82c9492d80.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.53ebc025.js"
   },
   {
-    "revision": "f38abe7d32754adaea56",
-    "url": "/portfolio/static/js/main.f38abe7d.chunk.js"
+    "revision": "2116d19c165089e566e1",
+    "url": "/portfolio/static/js/main.2116d19c.chunk.js"
   },
   {
     "revision": "8ebaeb2acf36d54792a7",
     "url": "/portfolio/static/js/2.8ebaeb2a.chunk.js"
   },
   {
-    "revision": "f38abe7d32754adaea56",
+    "revision": "2116d19c165089e566e1",
     "url": "/portfolio/static/css/main.8b1d7a64.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/portfolio/static/css/2.d64aa802.chunk.css"
   },
   {
-    "revision": "5b1eb83beb8172d2b931d93d4a9b02d6",
+    "revision": "d356da87327de98c5a42060ca2eb5eb4",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.67d5f90669abe7057617ae679a4b8a3f.js"
+  "/portfolio/precache-manifest.f2ac3948756bbd8b3b614d82c9492d80.js"
 );
 
 workbox.clientsClaim();
