6052372343717c5aebc3065914c713c359dce1bf
FranHainesZone <52914701+FranHainesZone@users.noreply.github.com>
2021-04-21 08:19:26 +0100

    Updated bio

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -40,23 +40,22 @@ export const Hero = () => {
                 <h2 css={h2}>A Front-end Developer based in Bristol.</h2>
                 <p>
                   Originally from a small town in the West Midlands, I decided
-                  to move to Bristol shortly after a stint living in Cardiff
-                  following on from university. I have never looked back since
-                  and I absolutely cannot imagine living anywhere else.
+                  to move to Bristol shortly after a stint living in Cardiff.
+                  I've never looked back since and I absolutely cannot imagine
+                  living anywhere else.
                 </p>
                 <p>
-                  I currently work at Zone - working with Vue, CSS and JS,
-                  mostly. I love my job and try to give back to the community by
+                  I work at Zone and I'm currently working with Vue, CSS and JS.
+                  I love my job and try to give back to the community by
                   mentoring at both Codebar and a local Bristol high school.
                 </p>
                 <p>
                   I love being outdoors - I'm really into running, cycling and
                   swimming. I've ran a handful of half marathons, a full
-                  marathon and I'm doing a Triathlon later this year (which is
-                  held in the Avon, so wish me luck with the swimming!). I also
-                  love video games - I'm a Playstation gal usually but have been
-                  playing a lot of my Switch recently. In a post-pandemic world
-                  you can usually find me at gigs in and around Bristol.
+                  marathon and I'm doing my first Triathlon later this year. I
+                  also love video games (Switch and Playstation) and reading
+                  (mainly feminism, sociology and sci-fi). In a post-pandemic
+                  world you can usually find me at gigs in and around Bristol.
                 </p>
               </div>
 
