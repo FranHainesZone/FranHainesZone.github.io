6a9b73c8e45473147197072891afae2f10a23388
FranHainesZone <52914701+FranHainesZone@users.noreply.github.com>
2021-06-04 15:01:10 +0100

    Updates

---
+++ precache-manifest.ab15eb866bb2ace128b48309bd28282d.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2897b0b2f3c73b60d39347f4f9b4da9",
+    "url": "/static/media/article-100-women-panel.a2897b0b.jpeg"
+  },
+  {
+    "revision": "7db3db67f2ed4823e2eb",
+    "url": "/static/css/main.6991c52c.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "762359adeadedf32ca4493c6086b69f7",
+    "url": "/static/media/icon-codepen.762359ad.svg"
+  },
+  {
+    "revision": "6b377d4f64947795ed76",
+    "url": "/static/js/2.6b377d4f.chunk.js"
+  },
+  {
+    "revision": "21eea559d9a6659b28b662884f23438e",
+    "url": "/static/media/hot-air-balloon.21eea559.svg"
+  },
+  {
+    "revision": "3d9b2ea1eeea8a37d27335fa4da319d3",
+    "url": "/static/media/giphy.3d9b2ea1.svg"
+  },
+  {
+    "revision": "f5ea2225bb21bf75b7eee4959957cbe1",
+    "url": "/static/media/movie-mania.f5ea2225.svg"
+  },
+  {
+    "revision": "66fac376c25e64cd040105935f2daba4",
+    "url": "/static/media/personal-assistant.66fac376.svg"
+  },
+  {
+    "revision": "150f97ba1c9ccb137ea857669fb37b8a",
+    "url": "/static/media/zone-website.150f97ba.svg"
+  },
+  {
+    "revision": "d603f8bfba0b888c46bed6ec4bed3133",
+    "url": "/static/media/article-learning-by-teaching.d603f8bf.jpg"
+  },
+  {
+    "revision": "7db3db67f2ed4823e2eb",
+    "url": "/static/js/main.7db3db67.chunk.js"
+  },
+  {
+    "revision": "ec7f36a324fe4d6adc581e9549b63103",
+    "url": "/static/media/article-zone-women-in-tech.ec7f36a3.jpeg"
+  },
+  {
+    "revision": "d48f0fc572e299a30e25eb23dd59b7af",
+    "url": "/static/media/article-women-of-silicon-roundabout.d48f0fc5.jpeg"
+  },
+  {
+    "revision": "e7f7ddb510d4ee26af9ba47acaeb6bfa",
+    "url": "/static/media/self-portrait.e7f7ddb5.svg"
+  },
+  {
+    "revision": "313dec4e638360f3bca630f16152e95a",
+    "url": "/static/media/shapes-first.313dec4e.svg"
+  },
+  {
+    "revision": "ab6ce540426b8165b8a38243e0df9c91",
+    "url": "/static/media/shapes-second.ab6ce540.svg"
+  },
+  {
+    "revision": "060b1fb6749682573015fe9825207490",
+    "url": "/static/media/shapes-third.060b1fb6.svg"
+  },
+  {
+    "revision": "631cd5664eb1d4a27681a21ca4ad6126",
+    "url": "/static/media/icon-twitter.631cd566.svg"
+  },
+  {
+    "revision": "406464ed11bd2c484a99ecc9870acb4a",
+    "url": "/static/media/icon-linkedin.406464ed.svg"
+  },
+  {
+    "revision": "1bb027109345a90a9eab1e929d8669c2",
+    "url": "/static/media/icon-github.1bb02710.svg"
+  },
+  {
+    "revision": "6b377d4f64947795ed76",
+    "url": "/static/css/2.d64aa802.chunk.css"
+  },
+  {
+    "revision": "198ddb885f545e4a9c7ff604678f69e7",
+    "url": "/index.html"
+  }
+];
